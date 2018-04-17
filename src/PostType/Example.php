0274ee458ebfba88d01e5d0f31aeea414b6a746d
Jason Adams <jason.the.adams@gmail.com>
2018-04-17 10:53:11 -0700

    example adjustments

--- src/PostType/Example.php
+++ src/PostType/Example.php
@@ -21,19 +21,19 @@ class Example extends BasePostType {
    */
   public function get_labels() {
     return [
-      'name'              => 'Examples',
-      'singular_name'     => 'Example',
-      'add_new'           => 'Add Example',
-      'add_new_item'      => 'Add New Example',
-      'edit_item'         => 'Edit Example',
-      'new_item'          => 'New Example',
-      'all_items'         => 'All Examples',
-      'view_item'         => 'View Example',
-      'search_items'      => 'Search Examples',
-      'not_found'         => 'No Examples Found',
-      'not_found_in_trash'=> 'No Examples found in Trash',
-      'parent_item_colon' => '',
-      'menu_name'         => 'Examples'
+      'name'               => 'Examples',
+      'singular_name'      => 'Example',
+      'add_new'            => 'Add Example',
+      'add_new_item'       => 'Add New Example',
+      'edit_item'          => 'Edit Example',
+      'new_item'           => 'New Example',
+      'all_items'          => 'All Examples',
+      'view_item'          => 'View Example',
+      'search_items'       => 'Search Examples',
+      'not_found'          => 'No Examples Found',
+      'not_found_in_trash' => 'No Examples found in Trash',
+      'parent_item_colon'  => '',
+      'menu_name'          => 'Examples'
     ];
   }
 
@@ -42,14 +42,15 @@ class Example extends BasePostType {
    */
   public function get_arguments() {
     return [
-  		'labels'			=> $this->get_labels(),
-  		'title'			=> __('Title'),
-  		'supports'		=> array( 'title', 'editor', 'excerpt' ),
-  		'public'		=> true,
-  		'has_archive'	=> true,
-  		'rewrite'		=> array( 'slug' => 'example' ),
-  		'capability_type'	=> 'post',
-  		'hide_meta_box'	=> array( 'slug' ),
+      'labels'              => $this->get_labels(),
+      'title'               => __( 'Title' ),
+      'supports'            => array( 'title', 'editor', 'excerpt' ),
+      'public'              => true,
+      'has_archive'         => true,
+      'rewrite'             => array( 'slug' => 'example' ),
+      'capability_type'     => 'post',
+      'hide_meta_box'       => array( 'slug' ),
+      'wp_controller_class' => 'Example'
     ];
   }
 }
