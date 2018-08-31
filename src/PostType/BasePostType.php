ddca463a367b5a9c74818f398142214bfcd5c36e
Jason Adams <jason.the.adams@gmail.com>
2018-08-31 12:16:18 -0700

    added support for easily displaying taxonomy filters in admin

--- src/PostType/BasePostType.php
+++ src/PostType/BasePostType.php
@@ -16,8 +16,14 @@ abstract class BasePostType implements Registerable
      */
     public function register()
     {
+        // Register Post Type
         add_action('init', [$this, 'register_post_type']);
+
+        // Setup templates
         $this->load($this->get_slug());
+
+        // Add taxonomy filters
+        add_action('restrict_manage_posts', [$this, 'add_taxonomy_filters'], 10, 2);
     }
 
     /**
@@ -26,6 +32,42 @@ abstract class BasePostType implements Registerable
     public function register_post_type()
     {
         register_post_type($this->get_slug(), $this->get_arguments());
+    }
+
+    /**
+     * @param string         $post_type
+     * @param \WP_List_Table $which
+     */
+    public function add_taxonomy_filters($post_type, $which)
+    {
+        if ($post_type !== $this->get_slug()) {
+            return;
+        }
+
+        $taxonomies = get_object_taxonomies($post_type, 'objects');
+
+        foreach ($taxonomies as $taxonomy => $object) {
+            if (!isset($object->show_admin_filter)) {
+                continue;
+            }
+
+            if (is_array($object->show_admin_filter) && !in_array($post_type, $object->show_admin_filter)) {
+                continue;
+            }
+
+            wp_dropdown_categories([
+                'show_option_all' => "All {$object->label}",
+                'taxonomy'        => $taxonomy,
+                'name'            => $taxonomy,
+                'orderby'         => 'name',
+                'selected'        => isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '',
+                'hierarchical'    => $object->hierarchical,
+                'depth'           => 3,
+                'show_count'      => true,
+                'hide_empty'      => false,
+                'value_field'     => 'slug'
+            ]);
+        }
     }
 
     /**
